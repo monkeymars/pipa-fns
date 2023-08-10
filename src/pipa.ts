edf37ba8ee21713ed2427a8af10342c3f44c1001
Gery Wibowo <gery.wibowo@lemonilo.com>
2023-08-10 23:44:27 +0700

    feat: pipe function

---
+++ src/pipa.ts
@@ -0,0 +1,12 @@
+type Func<T, R> = (arg: T) => R | Promise<R>;
+
+export default async function pipa(
+  ...fns: Func<any, any>[]
+): Promise<Func<any, Promise<any>>> {
+  return async (arg: any): Promise<any> => {
+    return fns.reduce(async (result, fn) => {
+      const previousResult = await result;
+      return fn(previousResult);
+    }, Promise.resolve(arg));
+  };
+}
---
+++ tests/main.test.ts
@@ -0,0 +1,53 @@
+import pipa from "../src/pipa";
+
+describe("pipa", () => {
+  it("should return a function that returns the input argument when no functions are provided", async () => {
+    const pipedFn = await pipa();
+    const result = await pipedFn("test");
+    expect(result).toBe("test");
+  });
+
+  it("should pipe the input argument through the provided functions", async () => {
+    const addOne = (num: number) => num + 1;
+    const multiplyByTwo = (num: number) => num * 2;
+    const subtractThree = (num: number) => num - 3;
+
+    const pipedFn = await pipa(addOne, multiplyByTwo, subtractThree);
+    const result = await pipedFn(5);
+    expect(result).toBe(9); // (5 + 1) * 2 - 3 = 7
+  });
+
+  it("should work with asynchronous functions", async () => {
+    const asyncAddOne = async (num: number) => {
+      return new Promise<number>((resolve) => {
+        setTimeout(() => {
+          resolve(num + 1);
+        }, 100);
+      });
+    };
+
+    const asyncMultiplyByTwo = async (num: number) => {
+      return new Promise<number>((resolve) => {
+        setTimeout(() => {
+          resolve(num * 2);
+        }, 100);
+      });
+    };
+
+    const asyncSubtractThree = async (num: number) => {
+      return new Promise<number>((resolve) => {
+        setTimeout(() => {
+          resolve(num - 3);
+        }, 100);
+      });
+    };
+
+    const pipedFn = await pipa(
+      asyncAddOne,
+      asyncMultiplyByTwo,
+      asyncSubtractThree
+    );
+    const result = await pipedFn(5);
+    expect(result).toBe(7); // (5 + 1) * 2 - 3 = 7
+  });
+});
