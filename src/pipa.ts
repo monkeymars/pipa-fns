ead6c63975507e105d7bd99bbd915069a41b00d1
Gery Wibowo <gery.wibowo@lemonilo.com>
2023-08-12 21:09:42 +0700

    ignoring dist

--- src/pipa.ts
+++ src/pipa.ts
@@ -1,8 +1,6 @@
 type Func<T, R> = (arg: T) => R | Promise<R>;
 
-export default async function pipe(
-  ...fns: Func<any, any>[]
-): Promise<Func<any, Promise<any>>> {
+export default async function pipe(...fns: Func<any, any>[]): Promise<Func<any, Promise<any>>> {
   return async (arg: any): Promise<any> => {
     return fns.reduce(async (result, fn) => {
       const previousResult = await result;
