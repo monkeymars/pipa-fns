9a3cace33ddb39e3c97f3d8eaede758311a77379
Gery Wibowo <gery.wibowo@lemonilo.com>
2023-08-12 21:11:19 +0700

    fix: linter

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,13 +1,13 @@
-import pipe from "../src/pipa";
+import pipe from '../src/pipa';
 
-describe("pipe", () => {
-  it("should return a function that returns the input argument when no functions are provided", async () => {
+describe('pipe', () => {
+  it('should return a function that returns the input argument when no functions are provided', async () => {
     const pipedFn = await pipe();
-    const result = await pipedFn("test");
-    expect(result).toBe("test");
+    const result = await pipedFn('test');
+    expect(result).toBe('test');
   });
 
-  it("should pipe the input argument through the provided functions", async () => {
+  it('should pipe the input argument through the provided functions', async () => {
     const addOne = (num: number) => num + 1;
     const multiplyByTwo = (num: number) => num * 2;
     const subtractThree = (num: number) => num - 3;
@@ -17,7 +17,7 @@ describe("pipe", () => {
     expect(result).toBe(9); // (5 + 1) * 2 - 3 = 7
   });
 
-  it("should work with asynchronous functions", async () => {
+  it('should work with asynchronous functions', async () => {
     const asyncAddOne = async (num: number) => {
       return new Promise<number>((resolve) => {
         setTimeout(() => {
@@ -42,12 +42,8 @@ describe("pipe", () => {
       });
     };
 
-    const pipedFn = await pipe(
-      asyncAddOne,
-      asyncMultiplyByTwo,
-      asyncSubtractThree
-    );
+    const pipedFn = await pipe(asyncAddOne, asyncMultiplyByTwo, asyncSubtractThree);
     const result = await pipedFn(5);
-    expect(result).toBe(7); // (5 + 1) * 2 - 3 = 7
+    expect(result).toBe(9); // (5 + 1) * 2 - 3 = 7
   });
 });
